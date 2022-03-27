22cee65953488c0ebc570382af8dd33eccc0942a
Abyzma <47283721+abyzma@users.noreply.github.com>
2022-03-27 01:08:22 +0100

    Fix Abyzma Method (#14)

--- public/calculators/abyzma.js
+++ public/calculators/abyzma.js
@@ -9,9 +9,13 @@ function abyzma(parameters, data) {
 	const maxQ2 = (1656612000000)
 	const Q2Result = Math.floor(Math.random() * (maxQ2 - minQ2 + 1)) + minQ2;
 	
-	const minAQ2 = (1656698400000)
-	const maxAQ2 = (3382365600000)
-	const AQ2Result = Math.floor(Math.random() * (maxAQ2 - minAQ2 + 1)) + minAQ2;
+	const minQ3 = (1656698400000)
+	const maxQ3 = (1664553600000)
+	const Q3Result = Math.floor(Math.random() * (maxQ3 - minQ3 + 1)) + minQ3;
+
+	const minAQ3 = (1664640000000)
+	const maxAQ3 = (3382365600000)
+	const AQ3Result = Math.floor(Math.random() * (maxAQ3 - minAQ3 + 1)) + minAQ3;
 	
 
     const millisInDay = 1000 * 60 * 60 * 24;
@@ -22,8 +26,12 @@ function abyzma(parameters, data) {
     if (parameters.order_avail == "Q2") {
         return new Date(Q2Result);
     }
-	if (parameters.order_avail == "After Q2"){
-		return new Date(AQ2Result);
+	if (parameters.order_avail == "Q3")
+	{
+		return new Date(Q3Result)
 	}
-    throw "bad state";
+	if (parameters.order_avail == "After Q3"){
+		return new Date(AQ3Result);
+	}
+    throw "no thanks";
 }
